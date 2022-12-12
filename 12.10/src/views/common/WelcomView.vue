5e08ad76349210357748f3a2eb2bd47016d770d8
lair <3519892925@qq.com>
2022-12-12 11:15:16 +0800

    12.10

---
+++ 12.10/src/views/common/WelcomView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <h1>welcoom!!!!</h1>
+  </div>
+</template>
+<style>
+.box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
