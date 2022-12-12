135079599a408efc57d5c1230fe33c5c910bb7a9
lair <3519892925@qq.com>
2022-12-12 11:12:02 +0800

    12.7

---
+++ 12.7/src/views/common/LoginView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="loginView">
+    <el-form class="form" ref="ruleForm" :model="form" :rules="rules">
+      <img src="../assets/sanjiubig.jpg" alt="" />
+      <el-form-item prop="username">
+        <el-input prefix-icon="el-icon-user" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input prefix-icon="el-icon-lock" v-model="form.password" type="password"></el-input>
+      </el-form-item>
+      <el-form-item class="btn">
+        <el-button type="primary" @click="submitForm">登录</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginView",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          // trigger 表示的是触发校验规则的方法 值是blur表示输入框失去焦点的时候进行校验
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 18, message: "请输入6～18位字符的用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, min: 6, max: 18, message: "请输入6～18位字符的用户名", trigger: "blur" }]
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      // console.log(this)
+      this.$refs["ruleForm"].validate((valid) => {
+        if (valid) {
+          this.$http({ url: this.$http.addorUrl("/login"), params: this.form, method: "post" }).then((res) => {
+            console.log(res)
+            this.$router.push("/")
+          })
+        } else {
+          console.log("error submit!!")
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.loginView {
+  // background-color: #3e4b6a;
+  background-image: url(../assets/1.gif);
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: 100% 100%;
+}
+.form {
+  width: 500px;
+  background-color: #fff;
+  padding: 100px 20px;
+  padding-bottom: 40px;
+  border-radius: 8px;
+  position: relative;
+}
+img {
+  position: absolute;
+  width: 130px;
+  height: 130px;
+  border-radius: 130px;
+  background-color: #ccc;
+  top: -50px;
+  left: 185px;
+  border: 10px solid #fff;
+  box-shadow: 5px 5px 10px #ccc;
+}
+.btn {
+  text-align: right;
+}
+</style>
